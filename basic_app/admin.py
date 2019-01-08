2ce0974d8e944865f0db97fcc178a5a3c3680090
leelu <leelunavy@gmail.com>
2019-01-07 21:00:23 -0500

    just applying some changes to see how things are going on

---
+++ basic_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from basic_app.models import UserProfileInfo
+# from learning_users. import basic_app
+# Register your models here.
+
+admin.site.register(UserProfileInfo)
+# admin.site.register(basic_app)
